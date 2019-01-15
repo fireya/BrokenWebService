911bb8e1f91aba534910aed891661a633c1dd584
Artem Syrovatskiy <asyrovatskiy@gmcs.ru>
2019-01-15 11:34:19 +0300

    init

---
+++ BrokenWebApplication/Controlers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace BrokenWebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public async Task<string> Get()
+        {
+            return Task.FromResult(_configuration.GetValue<string>("Config/Phrase"));
+        }
+    }
+}
